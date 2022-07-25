6fcecc607e37010ab41fa286ab3d91a417495dc4
Brock <Lynch.b@hotmail.com>
2022-07-25 12:23:18 -0400

    fixed bugs and made responsive

--- js/site.js
+++ js/site.js
@@ -1,16 +1,24 @@
 
 
 function getData(){
-    let amount = parseInt(document.getElementById("loanAmt").value);
+    let amount = parseFloat(document.getElementById("loanAmt").value);
 
     let term = parseInt(document.getElementById("term").value);
 
     let intRate = parseFloat(document.getElementById("intRate").value);
 
+    if(amount < 0 || term < 0 || intRate < 0 || !Number.isFinite(amount) || !Number.isFinite(term) || !Number.isFinite(intRate)){
+        Swal.fire({
+            backdrop: false,
+            title: "oops!",
+            text: "Please enter positive numbers."
+           });
+    }else{
+
     let data = createObj(amount, term, intRate);
 
     displayData(data,term);
-
+    }
 }
 
 function calculateMonthlyPayment(amount, term, rate){
@@ -44,6 +52,7 @@ function createObj(amount, term, rate){
         tableObj[index] = [];
         tableObj[index].month=index+1;
         tableObj[index].payment = calculateMonthlyPayment(amount,term,rate);
+
         if(index == 0){
             balance = amount;
             tableObj[index].totalInterest = interest(balance,rate);
@@ -51,10 +60,10 @@ function createObj(amount, term, rate){
             balance = tableObj[index-1].balance;
             tableObj[index].totalInterest = interest(balance,rate)+tableObj[index-1].totalInterest;
         }
+
         tableObj[index].principle=principle(tableObj[index].payment, interest(balance,rate));
         tableObj[index].interest = interest(balance,rate);
-        tableObj[index].balance = remainingBalance(balance,tableObj[index].principle);
-
+        tableObj[index].balance = Math.abs(remainingBalance(balance,tableObj[index].principle));
     }
 
     return tableObj;
@@ -65,8 +74,12 @@ function displayData(createObj,term){
     let dataTemplate = document.getElementById("data-template");
     let dataBody = document.getElementById("infoBody");
     dataBody.innerHTML = "";
+
     let monthly = document.getElementById("monthlyPayment");
-    monthly.innerHTML = createObj[0].payment.toLocaleString();
+    monthly.innerHTML = createObj[0].payment.toLocaleString("en-US",{
+        style: "currency",
+        currency: 'USD',
+    });
 
     let totalPrinciple = 0;
     let totalInterest = createObj[term-1].totalInterest;
@@ -76,23 +89,22 @@ function displayData(createObj,term){
     }
     document.getElementById("totalPrincipal").innerHTML=totalPrinciple.toLocaleString(
         "en-US",{
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
         }
     );
     document.getElementById("totalInterest").innerHTML = totalInterest.toLocaleString(
         "en-US",{
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
         }
     );
     document.getElementById("totalCost").innerHTML = totalCost.toLocaleString(
         "en-US",{
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
         }
     );
-
     
 
     for (let index = 0; index < term; index++) {
@@ -104,32 +116,32 @@ function displayData(createObj,term){
         dataItems[0].textContent = createObj[index].month.toLocaleString();
         dataItems[1].textContent = createObj[index].payment.toLocaleString(
             "en-US",{
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
             }
         );
         dataItems[2].textContent = createObj[index].principle.toLocaleString(
             "en-US",{
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
             }
         );
         dataItems[3].textContent = createObj[index].interest.toLocaleString(
             "en-US",{
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
             }
         );
         dataItems[4].textContent = createObj[index].totalInterest.toLocaleString(
             "en-US",{
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
             }
         );
         dataItems[5].textContent = createObj[index].balance.toLocaleString(
             "en-US",{
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
+            style: "currency",
+            currency: 'USD',
             }
         );
 
